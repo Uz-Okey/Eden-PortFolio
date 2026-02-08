10595892b1f6f5a4a586bbb814ad76f71595f424
Uzondu Okechukwu <uzonduokey30@gmail.com>
2026-02-08 03:21:30 +0100

    update the package.json

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://edenport-folio.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
